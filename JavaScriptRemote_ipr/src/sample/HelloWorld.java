becb363fbc092586832369fbbcb58e24aa4c342e
Andrey <andrey.dernov@gmail.com>
2013-02-18 00:23:49 +0400

    Remote debug

---
+++ JavaScriptRemote_ipr/src/sample/HelloWorld.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrey
+ * Date: 17.02.13
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static String getMessage(){
+        return "Hello World";
+    }
+}
